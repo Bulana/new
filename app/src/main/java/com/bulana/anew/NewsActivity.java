ddc567ae74d31caca54ddad6703dbe07af8ef44c
Bulana <moorbulana@gmail.com>
2019-08-21 08:06:21 +0200

    adding more news

--- app/src/main/java/com/bulana/anew/NewsActivity.java
+++ app/src/main/java/com/bulana/anew/NewsActivity.java
@@ -1,6 +1,11 @@
 package com.bulana.anew;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.NavigationView;
@@ -10,10 +15,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +30,18 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class NewsActivity extends AppCompatActivity {
+
+    //fields init
+    private RecyclerView recyclerView;
+    private ArticleAdapter articleAdapter;
+    public ArrayList<ArticleModel> mArticlesList;
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final String STATE_LIST = "Adapter data";
+
+    private View loadingView;
+    private View noDataImage;
+    public int networkCallsCounter;
 
     //fragment setup
     private FragmentManager fragmentManager;
@@ -79,6 +99,122 @@ public class NewsActivity extends AppCompatActivity {
                 assert drawer != null;
                 drawer.closeDrawer(GravityCompat.START);
                 return true;
+            }
+        });
+
+
+        loadingView = findViewById(R.id.loading_spinner);
+        noDataImage = findViewById(R.id.noData);
+     //   noDataImage.setVisibility(View.GONE);
+
+        //checking previously saved articles
+        if (savedInstanceState != null) {
+            mArticlesList = savedInstanceState.getParcelableArrayList(STATE_LIST);
+            networkCallsCounter = Integer.parseInt(savedInstanceState.getString("COUNTER"));
+            recyclerView = findViewById(R.id.recyclerView);
+
+            articleAdapter = new ArticleAdapter(NewsActivity.this, mArticlesList);
+            recyclerView.setLayoutManager(new LinearLayoutManager(NewsActivity.this));
+            recyclerView.setAdapter(articleAdapter);
+
+            //set spinner to invisible
+            loadingView.setVisibility(View.GONE);
+
+        } else {
+            initialise();
+        }
+
+    }
+
+    //Initializer for RecyclerView, Adapter
+    private void initialise() {
+        recyclerView = findViewById(R.id.recyclerView);
+        articleAdapter = new ArticleAdapter(NewsActivity.this, new ArrayList<ArticleModel>());
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(NewsActivity.this));
+        recyclerView.setAdapter(articleAdapter);
+    }
+
+    //internet connection check
+    private boolean isConnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+
+    //Activity Life Cycles
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+
+        //No Internet Connection dialog
+        if(!isConnected()){
+            new AlertDialog.Builder(this).setIcon(R.drawable.no_internet)
+                    .setTitle("Internet Connection Alert")
+                    .setMessage("Please Check your Internet")
+                    .setPositiveButton("Close", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int which) {
+                            noDataImage.setVisibility(View.VISIBLE);
+                        }
+                    })
+                    .show();
+        }
+
+        //set no data image to invisible
+        noDataImage.setVisibility(View.GONE);
+
+        //articleData obj, ArticleListAsyncResponse obj created
+        new ArticleData().getNewsList(new ArticleListAsyncResponse() {
+
+            @Override
+            public void processFinish(ArrayList<ArticleModel> articlesList) {
+
+                //First Simulation
+                //Set article data
+                loadingView.setVisibility(View.GONE);
+                articleAdapter.updateData(articlesList);
+                mArticlesList = articlesList;
+
+                //setting listener on adapter, view holder
+                articleAdapter.setOnClickListener(new ArticleAdapter.OnItemClickListener() {
+
+                    @Override
+                    public void onArticleSelected(ArticleModel articleData) {
+
+                        Intent intent = new Intent(NewsActivity.this, DetailsActivity.class);
+                        intent.putExtra("url", articleData.getNewsUrl());
+                        startActivity(intent);
+
+                    }
+                });
+
+                //Second Simulation
+                if (networkCallsCounter == 1) {
+                    ArticleModel article = new ArticleModel();
+                    article.setAuthor("AUTHOR");
+                    article.setTitle("TITLE");
+                    article.setDescription(("description"));
+                    article.setImageUrl("urlToImage");
+                    article.setPublishedDate("publishedAt");
+                    article.setNewsUrl("url");
+                    articlesList.add(0,article);
+                }
+
+                //Third Simulation
+                if(networkCallsCounter == 2){
+                    //clear data in list
+                    articlesList.clear();
+
+                    //check size
+                    if(articlesList.size() == 0){
+                        //show  no data image
+                        noDataImage.setVisibility(View.VISIBLE);
+                    }
+                }networkCallsCounter++;
             }
         });
 
